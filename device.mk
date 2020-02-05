0ddf52285cab8bb1fed5e7a1b2af99a6561b80ce
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-02-05 12:48:26 +0500

    grus: cleanup

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2020 The Havoc-OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -471,6 +471,7 @@ PRODUCT_PACKAGES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
+    libmediaextractorservice \
     libnl
 
 PRODUCT_BOOT_JARS += \
